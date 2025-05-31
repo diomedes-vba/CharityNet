a1d9a04ec32d52841dfddf060bd9c7813bc2ba6d
diomedes-vba <vladyslav.babych2106@gmail.com>
2025-05-31 15:59:29 +0400

    Control, navigator, viewmodels and styles

---
+++ CharityNet.UI/State/Navigators/INavigator.cs
@@ -0,0 +1,17 @@
+﻿using CharityNet.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CharityNet.UI.State.Navigators
+{
+    public enum ViewType { Home, YourDonations }
+    public interface INavigator
+    {
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
---
+++ CharityNet.UI/State/Navigators/Navigator.cs
@@ -0,0 +1,18 @@
+﻿using CharityNet.UI.Commands;
+using CharityNet.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CharityNet.UI.State.Navigators
+{
+    public class Navigator : INavigator
+    {
+        public ViewModelBase CurrentViewModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+    }
+}
